1487b0434a2e6ab88c761a6d58141f70a45a20b6
62011019 <p.pluemapirak@gmail.com>
2022-02-10 11:16:44 +0700

    .

---
+++ BASIC/01.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+      System.out.println("________________________");
+      System.out.println("Welcome Back To JAVA !!");
+      System.out.println("You Look So Happy.");
+      System.out.println(".");
+      System.out.println(".");
+      System.out.println(".");
+      System.out.println(".");
+      System.out.println("Just Have A Good Day !!");
+      System.out.println(".");
+      System.out.println(".");
+      System.out.println(".");
+      System.out.println(".");
+      System.out.println(":) :) :) :) :) :) :) :) :)");
+      System.out.println("________________________");
+    }
+  }
+  

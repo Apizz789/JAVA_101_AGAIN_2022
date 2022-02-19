674d6d066662e54568f1f8d575e14799442b2eed
APIRAK OULIS <82326608+Apizz789@users.noreply.github.com>
2022-02-19 22:34:38 +0700

    .

---
+++ BASIC/Input.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class ATM {
+    public static void main(String[] args) {
+        Scanner obj1 = new Scanner(System.in);
+        System.out.print("Enter Name : ");
+        
+        String userName = obj1.nextLine();
+        System.out.println("NAME : " + userName);
+        
+    }
+}
